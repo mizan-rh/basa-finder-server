9a1e224a91657d5ab594ae12b40ad705ef8dd35d
Mizanur Rahman <43827361+mizan-rh@users.noreply.github.com>
2025-04-25 19:07:10 +0600

    update cors for live client side

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://basa-finder-client-swart.vercel.app'],
     credentials: true,
   }),
 );
